9e9ea068c5c576738cab1fa306f37e7a2e7fd969
Mr Matsela <malefane@vut.ac.za>
2025-04-30 14:01:28 +0200

    Modified the App.Java file

--- src/main/java/com/vut/App.java
+++ src/main/java/com/vut/App.java
@@ -4,10 +4,8 @@ package com.vut;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Simple Maven Calculator!!!");
     }
 }
